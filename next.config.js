9c6bc60f3e9bbb12f424f5e98e690e734c91771d
udayRolla <104409890+udayRolla@users.noreply.github.com>
2026-02-24 23:59:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              connect-src 'self' https://*.supabase.co;
+              img-src 'self' data: https:;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval';
+              style-src 'self' 'unsafe-inline';
+            `.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
